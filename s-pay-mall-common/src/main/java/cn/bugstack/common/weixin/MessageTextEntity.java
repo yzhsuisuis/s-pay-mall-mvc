1daa0397bec671c9ac51434b9264e96a69d02521
yangbo <972849883@qq.com>
2024-11-30 14:26:10 +0800

    fix: 微信登录debug

--- s-pay-mall-common/src/main/java/cn/bugstack/common/weixin/MessageTextEntity.java
+++ s-pay-mall-common/src/main/java/cn/bugstack/common/weixin/MessageTextEntity.java
@@ -23,7 +23,7 @@ public class MessageTextEntity {
     @XStreamAlias("EventKey")
     private String eventKey;
 
-    @XStreamAlias("MsgId")
+    @XStreamAlias("MsgID")
     private String msgId;
 
     @XStreamAlias("Status")
